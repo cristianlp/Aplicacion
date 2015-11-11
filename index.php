f023d97b527f8500bb2453aaea17ddea363760c5
screenfun <cristhian.leonlizarazo@gmail.com>
2015-11-11 10:24:18 -0500

    estructura

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Santo Sasoft - Restaurante Bar</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="Estatico/css/bootstrap.min.css">
+	<link rel="stylesheet" href="Estatico/css/estilo.css">
+</head>
+<body>
+	
+	<script src="Estatico/js/jquery-1.11.3.min.js"></script>
+	<script src="Estatico/js/bootstrap.min.js"></script>
+</body>
+
+</html>
